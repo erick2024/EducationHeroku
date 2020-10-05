e07d9d6b2f8136d43e93938c5b7962a7bc61d37a
erick2024 <71725468+erick2024@users.noreply.github.com>
2020-10-05 09:05:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,59 +7,67 @@ import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/BWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+# Load Data
 url = 'https://raw.githubusercontent.com/erick2024/educationstatistics/master/Education%20World%20Bank%20Stacked%20Area%20Graph%20Data%20-%20Sheet1.csv'
-df = pd.read_csv (url, index_col = 'Year')
+df = pd.read_csv(url, index_col = 'Year').stack()
+df = df.reset_index()
+df.columns = ['Year', 'Region', 'Value']
 
-app = dash.Dash (__name__, external_stylesheets = external_stylesheets)
+# Build App
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-app.layout = html.Div ([
-    html.H3 ('Gross Intake Rate at Grade 1'),
-        html.Div ([
-        dcc.Dropdown (
-            id = 'Region', clearable = False,
-            value = 'North America', options = [
-                {'label' : c, 'value' : c}
-                for c in df.columns
+app.layout = html.Div([
+    html.H3("Gross Intake Rate at Grade 1"),
+        html.Div([
+        dcc.Dropdown(
+            id='region',
+            value=['North America'], options=[
+                {'label': c, 'value': c}
+                for c in df.Region.unique()
             ], multi = True),
-        ], style = {'display' : 'inline', 'width' : '15%'}),
-
-        html.Div ([
-        dcc.Graph (id = 'graph'),
-    ], style = {'display' : 'inline-block', 'width' : '45%'}),
-
-        html.Div ([
-        dcc.Graph (id = 'graph_2'),
-    ], style = {'display' : 'inline-block', 'width' : '55%'})
-
+    ],style={'display': 'inline', 'width': '15%'}),
+        
+        html.Div([
+        dcc.Graph(id='graph'),
+    ],style={'display': 'inline-block', 'width': '45%'}),
+        
+        html.Div([
+        dcc.Graph(id='graph_2'),
+    ],style={'display': 'inline-block', 'width': '55%'})
 ])
 
-app.callback (
-    [dash.dependencies.Output ('graph', 'figure'), dash.dependencies.Output ('graph_2', 'figure')],
-    [dash.dependencies.Input ('region', 'value')]
+# Define callback to update graph
+@app.callback(
+    [dash.dependencies.Output('graph', 'figure'),dash.dependencies.Output('graph_2', 'figure')],
+    [dash.dependencies.Input('region', 'value')]
 )
 
-def multi_output (region) :
 
-  fig1 = px.line (df, x = df.index, y = region)
-  fig2 = px.line (df, x = df.index, y = region)
+def multi_output(region):
 
-  fig1.update_layout (
-  yaxis_title = 'Gross Intake Rate at Grade 1',
-  showlegend = False
-  )
+    df_query = df.query('Region == '+str(region))
 
-  fig2.update_layout (
-  legend_title_text = 'Region',
-  yaxis_title = 'Gross Intake Rate at Grade 1'
-  )  
+    fig1 = px.line(df_query, x='Year', y='Value', color = 'Region')
+    fig2 = px.area(df_query, x='Year', y='Value', color = 'Region')
+    
+    fig1.update_layout(
+    yaxis_title='Gross Intake Rate at Grade 1',
+    showlegend = False,
+    )
+    
+    fig2.update_layout(
+    yaxis_title='Gross Intake Rate at Grade 1',
+    )
 
-  fig1.update_xaxes (showspikes = True)
-  fig1.update_yaxes (showspikes = True)
+    fig1.update_xaxes(showspikes=True)
+    fig1.update_yaxes(showspikes=True)
 
-  return fig1, fig2
+    return fig1, fig2
 
-if __name__ == '__main__' :
-  app.run_server ()
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(debug = True)
